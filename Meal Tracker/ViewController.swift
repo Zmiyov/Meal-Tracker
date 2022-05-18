d99238fa412bc64aad8f99798054a34759e1b77d
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-05-18 11:02:26 +0300

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Vladimir Pisarenko on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
